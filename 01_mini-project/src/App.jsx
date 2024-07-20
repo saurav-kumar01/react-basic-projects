f41f94c9a967af593c7d71caecbf134fbc921462
Saurav Kumar <saurabhmahur1098@gmail.com>
2024-07-20 14:43:17 +0530

    somthing change

--- 01_mini-project/src/App.jsx
+++ 01_mini-project/src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextFrom from "./components/TextFrom";
 
 function App() {
+  const [bgChange, setBgChange] = useState("black")
+  const toggleMode = () => {
+    if (mode === "light") {
+      setBgChange("white")
+      setMode("dark")
+    }else{
+      setBgChange("black")
+      setMode("light")
+    }
+    
+  }
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} bgChange={bgChange} toggleMode={toggleMode} />
       {/* <Navbar /> */}
       <div className="container my-3">
         <TextFrom heading="Fill this form required for login" />
--- 01_mini-project/src/components/Navbar.jsx
+++ 01_mini-project/src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import PropTypes from "prop-types";
 export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             {props.title}
@@ -44,6 +46,18 @@ export default function Navbar(props) {
                 Search
               </button>
             </form>
+            <div className={`form-check form-switch text-${props.bgChange} mx-3`}>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckChecked"
+                onClick={props.toggleMode}
+              />
+              <label className="form-check-label" htmlFor="flexSwitchCheckChecked">
+                Enable to Change Mode
+              </label>
+            </div>
           </div>
         </div>
       </nav>
